9d0f7d1d4e638e96963f6ea5e362e6d2d4126251
Jonathan Chu <jonathan.chu@me.com>
2015-05-28 01:43:30 -0400

    Add directories to exclude from `recentf`.

--- init.el
+++ init.el
@@ -399,7 +399,11 @@
 (require 'recentf)
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
-(add-to-list 'recentf-exclude "^/var/folders\\.*")
+(setq recentf-exclude '("^/var/folders\\.*"
+                        "COMMIT_EDITMSG\\'"
+                        ".*-autoloads\\.el\\'"
+                        "[/\\]\\.elpa/"
+                        ))
 
 
 ;;----------------------------------------------------------------------------
