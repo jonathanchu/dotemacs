6cbb12cfd43155d4dd5bd83632b0bebf0edcbdee
Jonathan Chu <jonathan.chu@me.com>
2015-05-31 23:00:49 -0400

    Update package archive list.

--- init.el
+++ init.el
@@ -48,10 +48,9 @@
 
 (require 'package)
 
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
 
 (package-initialize)
 
