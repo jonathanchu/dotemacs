05e9f44d13d0b5aa98872b6d923f31b6e39ff544
Jonathan Chu <jonathan.chu@me.com>
2015-06-08 10:22:36 -0400

    Add markdown-mode to packages list and its config.

--- init.el
+++ init.el
@@ -79,6 +79,7 @@
                       ido-ubiquitous
                       ido-vertical-mode
                       magit
+                      markdown-mode
                       nav
                       paredit
                       projectile
@@ -426,6 +427,13 @@
 ;; expand-region
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
+
+;; markdown-mode
+(require 'markdown-mode)
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 
 ;;----------------------------------------------------------------------------
