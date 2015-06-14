1bdd84cb25065ed6c3b8b999068b39a552644be3
Jonathan Chu <jonathan.chu@me.com>
2015-06-13 23:17:09 -0400

    Enable paredit for lisp modes.

--- init.el
+++ init.el
@@ -454,6 +454,16 @@ opening 4clojure questions"
 (require 'smartparens-config)
 (sp-local-pair 'web-mode "{%" "%}")
 
+;; paredit-mode
+(autoload 'enable-paredit-mode "paredit"
+  "Turn on pseudo-structural editing of Lisp code."
+  t)
+(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
+(add-hook 'ielm-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+(add-hook 'scheme-mode-hook 'enable-paredit-mode)
 
 ;;----------------------------------------------------------------------------
 ;; Defuns
