57d29407cdd12aa73762fd4f56a0ee1590a59845
Jonathan Chu <jonathan.chu@me.com>
2015-07-18 00:05:32 -0400

    Remove marmalade package archive

--- init.el
+++ init.el
@@ -49,7 +49,6 @@
 (require 'package)
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 
 (package-initialize)
