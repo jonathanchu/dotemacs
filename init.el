af7ac3448333635e818a7e6784bf70d7b88a8911
Jonathan Chu <jonathan.chu@me.com>
2015-08-02 12:35:42 -0500

    Make keybindings consistent for readability

--- init.el
+++ init.el
@@ -490,8 +490,8 @@ opening 4clojure questions"
 ;; swiper
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
-(global-set-key "\C-s" 'swiper)
-(global-set-key "\C-r" 'swiper)
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "C-r") 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 
 ;; ace-window
