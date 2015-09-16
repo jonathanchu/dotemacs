b019a354878ec1817613cf45460afdf41cbf2fba
Jonathan Chu <jonathan.chu@me.com>
2015-09-16 00:06:32 -0400

    Add function to copy filename and path to kill ring

--- init.el
+++ init.el
@@ -815,6 +815,25 @@ point reaches the beginning or end of the buffer, stop there."
               (newline))
           (insert (current-time-microseconds))))))
 
+;; Copy the buffer filename to the kill ring
+(defun copy-buffer-file-name-as-kill (choice)
+  "Copy the buffer-file-name to the kill-ring"
+  (interactive "cCopy Buffer Name (f) full, (p) path, (n) name")
+  (let ((new-kill-string)
+        (name (if (eq major-mode 'dired-mode)
+                  (dired-get-filename)
+                (or (buffer-file-name) ""))))
+    (cond ((eq choice ?f)
+           (setq new-kill-string name))
+          ((eq choice ?p)
+           (setq new-kill-string (file-name-directory name)))
+          ((eq choice ?n)
+           (setq new-kill-string (file-name-nondirectory name)))
+          (t (message "Quit")))
+    (when new-kill-string
+      (message "%s copied" new-kill-string)
+      (kill-new new-kill-string))))
+
 
 ;;----------------------------------------------------------------------------
 ;; Key Bindings
