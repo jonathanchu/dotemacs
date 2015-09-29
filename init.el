9b1c8643dc4ece5ea138bbfe5ba87776fc19397c
Jonathan Chu <jonathan.chu@me.com>
2015-09-28 22:36:11 -0400

    Add tangled web mode to init org file

--- init.el
+++ init.el
@@ -318,22 +318,22 @@
 ;;                (eldoc-mode 1))))
 
 ;; web-mode
-(use-package web-mode
-  :ensure
-  :config
-  (progn
-    (setq web-mode-markup-indent-offset 2)
-    (setq web-mode-css-indent-offset 2)
-    (setq web-mode-code-indent-offset 2)
-    (add-to-list 'auto-mode-alist '("\\.hb\\.html\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))))
+;; (use-package web-mode
+;;   :ensure
+;;   :config
+;;   (progn
+;;     (setq web-mode-markup-indent-offset 2)
+;;     (setq web-mode-css-indent-offset 2)
+;;     (setq web-mode-code-indent-offset 2)
+;;     (add-to-list 'auto-mode-alist '("\\.hb\\.html\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))))
 
 ;; makefile-mode
 (use-package makefile-mode
