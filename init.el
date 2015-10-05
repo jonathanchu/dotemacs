9810c5ecae38cce63650a0c61f3aeb9b937111dd
Jonathan Chu <jonathan.chu@me.com>
2015-10-05 12:18:52 -0400

    Add tangled ido mode to init org file

--- init.el
+++ init.el
@@ -278,14 +278,14 @@
 ;;   :ensure)
 
 ;; ido-mode
-(use-package ido
-  :config
-  (progn
-    (ido-mode t)
-    (ido-everywhere t)
-    (flx-ido-mode t)
-    (setq ido-enable-flex-matching t)
-    (setq ido-use-faces nil)))
+;; (use-package ido
+;;   :config
+;;   (progn
+;;     (ido-mode t)
+;;     (ido-everywhere t)
+;;     (flx-ido-mode t)
+;;     (setq ido-enable-flex-matching t)
+;;     (setq ido-use-faces nil)))
 
 ;; ido-vertical-mode
 (use-package ido-vertical-mode
