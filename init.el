bd0818d5f9598b8f951fb8535e830cf93e5088c5
Jonathan Chu <jonathan.chu@me.com>
2015-10-09 00:09:54 -0400

    Add elpy package and config

--- init.el
+++ init.el
@@ -648,6 +648,12 @@
     (cljr-add-keybindings-with-prefix "C-c C-m"))
   (add-hook 'clojure-mode-hook 'my-clojure-mode-hook))
 
+;; elpy
+(use-package elpy
+  :ensure
+  :config
+  (elpy-enable))
+
 ;; company-mode and backends
 ;; (use-package company
 ;;   :ensure
