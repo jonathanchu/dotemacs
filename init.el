87f54f68fe3728292df681209345e09603fb8ca1
Jonathan Chu <jonathan.chu@me.com>
2015-11-11 01:40:16 -0500

    Add tangled paredit mode to init org file

--- init.el
+++ init.el
@@ -566,19 +566,19 @@
 ;;     (context 2)))
 
 ;; paredit-mode
-(use-package paredit
-  :ensure
-  :config
-  (autoload 'enable-paredit-mode "paredit" t)
-  (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
-  (add-hook 'ielm-mode-hook 'enable-paredit-mode)
-  (add-hook 'lisp-mode-hook 'enable-paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
-  (add-hook 'scheme-mode-hook 'enable-paredit-mode)
-  (add-hook 'clojure-mode-hook 'enable-paredit-mode)
-  (add-hook 'python-mode-hook
-            (lambda () (local-set-key (kbd "C-k") 'paredit-kill))))
+;; (use-package paredit
+;;   :ensure
+;;   :config
+;;   (autoload 'enable-paredit-mode "paredit" t)
+;;   (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+;;   (add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
+;;   (add-hook 'ielm-mode-hook 'enable-paredit-mode)
+;;   (add-hook 'lisp-mode-hook 'enable-paredit-mode)
+;;   (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+;;   (add-hook 'scheme-mode-hook 'enable-paredit-mode)
+;;   (add-hook 'clojure-mode-hook 'enable-paredit-mode)
+;;   (add-hook 'python-mode-hook
+;;             (lambda () (local-set-key (kbd "C-k") 'paredit-kill))))
 
 ;; latex-preview-pane
 (use-package latex-preview-pane
