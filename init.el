139638f147721aa622fc38e4863d3310fcb0a79a
Jonathan Chu <jonathan.chu@me.com>
2015-11-20 00:12:59 -0500

    Add tangled flyspell to init org file

--- init.el
+++ init.el
@@ -627,9 +627,9 @@
 ;;   (autoload 'scratch "scratch" nil t))
 
 ;; flyspell
-(use-package flyspell
-  :config
-  (add-hook 'text-mode-hook 'flyspell-mode))
+;; (use-package flyspell
+;;   :config
+;;   (add-hook 'text-mode-hook 'flyspell-mode))
 
 ;; anzu
 (use-package anzu
