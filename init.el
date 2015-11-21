f8f359849016c583eea89b7c8568da61bdd27521
Jonathan Chu <jonathan.chu@me.com>
2015-11-21 00:11:53 -0500

    Add func for toggling window split and keybinding

--- init.el
+++ init.el
@@ -941,6 +941,32 @@ point reaches the beginning or end of the buffer, stop there."
   (if (looking-at "$")
       (backward-word 1)))
 
+;; toggle window split
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
 
 ;;----------------------------------------------------------------------------
 ;; Key Bindings
@@ -978,6 +1004,9 @@ point reaches the beginning or end of the buffer, stop there."
 
 ;; switch to previous buffer
 (global-set-key (kbd "C-`") 'switch-to-previous-buffer)
+
+;; toggle window split
+(global-set-key (kbd "C-x |") 'toggle-window-split)
 
 
 ;;----------------------------------------------------------------------------
