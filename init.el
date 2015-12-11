ee20ef8a7a12a420c21dc1b530fe870596ef4a12
Jonathan Chu <jonathan.chu@me.com>
2015-12-11 00:03:37 -0500

    Add smarter move to beginning of line to init org

--- init.el
+++ init.el
@@ -797,26 +797,26 @@
 ;;   (forward-char -1))
 
 ;; smarter navigation to the beginning of a line
-(defun smarter-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
-Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there."
-  (interactive "^p")
-  (setq arg (or arg 1))
+;; (defun smarter-move-beginning-of-line (arg)
+;;   "Move point back to indentation of beginning of line.
+;; Move point to the first non-whitespace character on this line.
+;; If point is already there, move to the beginning of the line.
+;; Effectively toggle between the first non-whitespace character and
+;; the beginning of the line.
+;; If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+;; point reaches the beginning or end of the buffer, stop there."
+;;   (interactive "^p")
+;;   (setq arg (or arg 1))
 
-  ;; Move lines first
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
+;;   ;; Move lines first
+;;   (when (/= arg 1)
+;;     (let ((line-move-visual nil))
+;;       (forward-line (1- arg))))
 
-  (let ((orig-point (point)))
-    (back-to-indentation)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
+;;   (let ((orig-point (point)))
+;;     (back-to-indentation)
+;;     (when (= orig-point (point))
+;;       (move-beginning-of-line 1))))
 
 ;; Highlight the call to ipdb
 ;; src http://pedrokroger.com/2010/07/configuring-emacs-as-a-python-ide-2/
@@ -986,7 +986,7 @@ point reaches the beginning or end of the buffer, stop there."
 (define-key global-map (kbd "s--") 'text-scale-decrease)
 
 ;; remap C-a to `smarter-move-beginning-of-line'
-(global-set-key [remap move-beginning-of-line] 'smarter-move-beginning-of-line)
+;; (global-set-key [remap move-beginning-of-line] 'smarter-move-beginning-of-line)
 
 ;; recentf with ido selection
 ;; bind to infrequently used find-file-read-only.
