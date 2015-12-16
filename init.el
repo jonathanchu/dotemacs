e919070a98482f196d22cc2b7392bb85b6f0fb38
Jonathan Chu <jonathan.chu@me.com>
2015-12-16 00:03:01 -0500

    Move timestamps for messages buffer function to init org file

--- init.el
+++ init.el
@@ -887,20 +887,20 @@
 
 ;; timestamps in *Messages*
 ;; via http://www.reddit.com/r/emacs/comments/1auqgm/speeding_up_your_emacs_startup/
-(defun current-time-microseconds ()
-  (let* ((nowtime (current-time))
-         (now-ms (nth 2 nowtime)))
-    (concat (format-time-string "[%Y-%m-%dT%T" nowtime) (format ".%d] " now-ms))))
+;; (defun current-time-microseconds ()
+;;   (let* ((nowtime (current-time))
+;;          (now-ms (nth 2 nowtime)))
+;;     (concat (format-time-string "[%Y-%m-%dT%T" nowtime) (format ".%d] " now-ms))))
 
-(defadvice message (before test-symbol activate)
-  (if (not (string-equal (ad-get-arg 0) "%s%s"))
-      (let ((inhibit-read-only t)
-            (deactivate-mark nil))
-        (with-current-buffer "*Messages*"
-          (goto-char (point-max))
-          (if (not (bolp))
-              (newline))
-          (insert (current-time-microseconds))))))
+;; (defadvice message (before test-symbol activate)
+;;   (if (not (string-equal (ad-get-arg 0) "%s%s"))
+;;       (let ((inhibit-read-only t)
+;;             (deactivate-mark nil))
+;;         (with-current-buffer "*Messages*"
+;;           (goto-char (point-max))
+;;           (if (not (bolp))
+;;               (newline))
+;;           (insert (current-time-microseconds))))))
 
 ;; Copy the buffer filename to the kill ring
 (defun copy-buffer-file-name-as-kill (choice)
