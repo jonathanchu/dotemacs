f4c44792f6101056359a9694a64c38ccf283c09f
Jonathan Chu <jonathan.chu@me.com>
2015-12-22 22:30:41 -0500

    Move over toggle window split function to init org

--- init.el
+++ init.el
@@ -945,30 +945,30 @@
 ;;       (backward-word 1)))
 
 ;; toggle window split
-(defun toggle-window-split ()
-  (interactive)
-  (if (= (count-windows) 2)
-      (let* ((this-win-buffer (window-buffer))
-             (next-win-buffer (window-buffer (next-window)))
-             (this-win-edges (window-edges (selected-window)))
-             (next-win-edges (window-edges (next-window)))
-             (this-win-2nd (not (and (<= (car this-win-edges)
-                                         (car next-win-edges))
-                                     (<= (cadr this-win-edges)
-                                         (cadr next-win-edges)))))
-             (splitter
-              (if (= (car this-win-edges)
-                     (car (window-edges (next-window))))
-                  'split-window-horizontally
-                'split-window-vertically)))
-        (delete-other-windows)
-        (let ((first-win (selected-window)))
-          (funcall splitter)
-          (if this-win-2nd (other-window 1))
-          (set-window-buffer (selected-window) this-win-buffer)
-          (set-window-buffer (next-window) next-win-buffer)
-          (select-window first-win)
-          (if this-win-2nd (other-window 1))))))
+;; (defun toggle-window-split ()
+;;   (interactive)
+;;   (if (= (count-windows) 2)
+;;       (let* ((this-win-buffer (window-buffer))
+;;              (next-win-buffer (window-buffer (next-window)))
+;;              (this-win-edges (window-edges (selected-window)))
+;;              (next-win-edges (window-edges (next-window)))
+;;              (this-win-2nd (not (and (<= (car this-win-edges)
+;;                                          (car next-win-edges))
+;;                                      (<= (cadr this-win-edges)
+;;                                          (cadr next-win-edges)))))
+;;              (splitter
+;;               (if (= (car this-win-edges)
+;;                      (car (window-edges (next-window))))
+;;                   'split-window-horizontally
+;;                 'split-window-vertically)))
+;;         (delete-other-windows)
+;;         (let ((first-win (selected-window)))
+;;           (funcall splitter)
+;;           (if this-win-2nd (other-window 1))
+;;           (set-window-buffer (selected-window) this-win-buffer)
+;;           (set-window-buffer (next-window) next-win-buffer)
+;;           (select-window first-win)
+;;           (if this-win-2nd (other-window 1))))))
 
 
 ;;----------------------------------------------------------------------------
@@ -1009,7 +1009,7 @@
 ;; (global-set-key (kbd "C-`") 'switch-to-previous-buffer)
 
 ;; toggle window split
-(global-set-key (kbd "C-x |") 'toggle-window-split)
+;; (global-set-key (kbd "C-x |") 'toggle-window-split)
 
 
 ;;----------------------------------------------------------------------------
