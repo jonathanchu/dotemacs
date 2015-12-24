1e5fc473ff8b41c487c8d720d95135b7d50434a1
Jonathan Chu <jonathan.chu@me.com>
2015-12-24 00:07:09 -0500

    Add faster pop-to-mark command

--- init.el
+++ init.el
@@ -970,6 +970,19 @@
 ;;           (select-window first-win)
 ;;           (if this-win-2nd (other-window 1))))))
 
+;; When popping the mark, continue popping until the cursor actually moves
+;; Also, if the last command was a copy - skip past all the expand-region cruft.
+(defadvice pop-to-mark-command (around ensure-new-position activate)
+  (let ((p (point)))
+    (when (eq last-command 'kill-ring-save)
+      ad-do-it
+      ad-do-it
+      ad-do-it)
+    (dotimes (i 10)
+      (when (= p (point)) ad-do-it))))
+
+(setq set-mark-command-repeat-pop t)
+
 
 ;;----------------------------------------------------------------------------
 ;; Key Bindings
