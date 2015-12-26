3e919c7a9b3e9e575ccf214c6250e7c1998705e0
Jonathan Chu <jonathan.chu@me.com>
2015-12-26 00:02:38 -0500

    Move pop to mark helper command to init org file

--- init.el
+++ init.el
@@ -972,16 +972,16 @@
 
 ;; When popping the mark, continue popping until the cursor actually moves
 ;; Also, if the last command was a copy - skip past all the expand-region cruft.
-(defadvice pop-to-mark-command (around ensure-new-position activate)
-  (let ((p (point)))
-    (when (eq last-command 'kill-ring-save)
-      ad-do-it
-      ad-do-it
-      ad-do-it)
-    (dotimes (i 10)
-      (when (= p (point)) ad-do-it))))
+;; (defadvice pop-to-mark-command (around ensure-new-position activate)
+;;   (let ((p (point)))
+;;     (when (eq last-command 'kill-ring-save)
+;;       ad-do-it
+;;       ad-do-it
+;;       ad-do-it)
+;;     (dotimes (i 10)
+;;       (when (= p (point)) ad-do-it))))
 
-(setq set-mark-command-repeat-pop t)
+;; (setq set-mark-command-repeat-pop t)
 
 
 ;;----------------------------------------------------------------------------
