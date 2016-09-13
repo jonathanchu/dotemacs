4b5d80ff3b6ebb54fe723cf44371fa807097bf93
Jonathan Chu <jonathan.chu@me.com>
2016-09-13 01:33:25 -0400

    Add in custom doom modeline
    All credit goes to @hlissner

--- init.el
+++ init.el
@@ -76,9 +76,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
  '(magit-branch-arguments nil)
  '(paradox-github-token t))
 (custom-set-faces
@@ -104,5 +101,5 @@
     (org-babel-load-file
      (expand-file-name "emacs-init.org" user-emacs-directory))))
 
-
+(load-file "~/.emacs.d/doom.el")
 ;;; init.el ends here
