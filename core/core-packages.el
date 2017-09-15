06687a2dd866e900eb48c4f307342af08d136740
Jonathan Chu <jonathan.chu@me.com>
2017-09-15 15:39:09 -0400

    Add trailing slash to melpa stable URL

--- core/core-packages.el
+++ core/core-packages.el
@@ -7,7 +7,7 @@
       package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
         ("melpa" . "https://melpa.org/packages/")
-        ("melpa-stable" . "https://stable.melpa.org/packages")))
+        ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 (package-initialize)
 
