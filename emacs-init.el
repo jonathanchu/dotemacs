abc7c1385c72214d8dfa87421ba2f5d1322427fb
Jonathan Chu <jonathan.chu@me.com>
2017-11-23 10:15:36 -0500

    Handle temp files again

--- emacs-init.el
+++ emacs-init.el
@@ -983,17 +983,17 @@ The CHAR is replaced and the point is put before CHAR."
       (move-beginning-of-line 1))))
 
 ;; Write temp files to directory to not clutter the filesystem
-;; (defvar user-temporary-file-directory
-;;   (concat temporary-file-directory user-login-name "/"))
-;; (make-directory user-temporary-file-directory t)
-;; (setq backup-by-copying t)
-;; (setq backup-directory-alist
-;;       `(("." . ,user-temporary-file-directory)
-;;         (,tramp-file-name-regexp nil)))
-;; (setq auto-save-list-file-prefix
-;;       (concat user-temporary-file-directory ".auto-saves-"))
-;; (setq auto-save-file-name-transforms
-;;       `((".*" ,user-temporary-file-directory t)))
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t)
+(setq backup-directory-alist
+      `(("." . ,user-temporary-file-directory)
+        (,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
 
 ;; duplicate the current line function
 (defun duplicate-line ()
