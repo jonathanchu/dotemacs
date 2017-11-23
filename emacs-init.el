1fc6f34544a341a1b0919ed967dbac3c05a45875
Jonathan Chu <jonathan.chu@me.com>
2017-11-23 10:15:26 -0500

    Add commented out solaire mode for now

--- emacs-init.el
+++ emacs-init.el
@@ -16,6 +16,49 @@
 (require 'doom-neotree)
 (setq doom-neotree-enable-file-icons t)
 
+;; (use-package solaire-mode
+;;   :ensure t)
+
+;; (require 'solaire-mode)
+
+;; ;; brighten buffers (that represent real files)
+;; (add-hook 'after-change-major-mode-hook #'turn-on-solaire-mode)
+;; ;; To enable solaire-mode unconditionally for certain modes:
+;; (add-hook 'ediff-prepare-buffer-hook #'solaire-mode)
+
+;; ;; ...if you use auto-revert-mode:
+;; (add-hook 'after-revert-hook #'turn-on-solaire-mode)
+
+;; ;; highlight the minibuffer when it is activated:
+;; (add-hook 'minibuffer-setup-hook #'solaire-mode-in-minibuffer)
+
+;; ;; if the bright and dark background colors are the wrong way around, use this
+;; ;; to switch the backgrounds of the `default` and `solaire-default-face` faces.
+;; ;; This should be used *after* you load the active theme!
+;; ;;
+;; ;; NOTE: This is necessary for themes in the doom-themes package!
+;; (solaire-mode-swap-bg)
+
+;; (require 'doom-themes)
+
+;; ;; Global settings (defaults)
+;; (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;;       doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; ;; may have their own settings.
+;; (load-theme 'doom-one t)
+
+;; ;; Enable flashing mode-line on errors
+;; (doom-themes-visual-bell-config)
+
+;; ;; Enable custom neotree theme
+;; (doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+
+;; ;; Corrects (and improves) org-mode's native fontification.
+;; (doom-themes-org-config)
+
+
 (use-package powerline
   :ensure t)
 
