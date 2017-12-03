1b091aa65a46bf9e35996bf9096526aa4122c37c
Jonathan Chu <jonathan.chu@me.com>
2017-12-03 01:22:17 -0500

    Move core back into init module

--- core/core.el
+++ core/core.el
@@ -4,36 +4,36 @@
   "Where the core files are stored.")
 
 ;; set encoding
-(prefer-coding-system 'utf-8)
+;; (prefer-coding-system 'utf-8)
 
-;; and tell emacs to play nice with encoding
-(define-coding-system-alias 'UTF-8 'utf-8)
-(define-coding-system-alias 'utf8 'utf-8)
+;; ;; and tell emacs to play nice with encoding
+;; (define-coding-system-alias 'UTF-8 'utf-8)
+;; (define-coding-system-alias 'utf8 'utf-8)
 
 ;; debugging
 ;; (setq debug-on-error t)
 
-;; save nothing
-(setq auto-save-default nil
-      create-lockfiles nil)
+;; ;; save nothing
+;; (setq auto-save-default nil
+;;       create-lockfiles nil)
 
-;; no splash screen
-(setq inhibit-splash-screen t)
+;; ;; no splash screen
+;; (setq inhibit-splash-screen t)
 
-;; no message on startup
-(setq initial-scratch-message nil)
+;; ;; no message on startup
+;; (setq initial-scratch-message nil)
 
 ;; Bootstrap
-(require 'cl-lib)
+;; (require 'cl-lib)
 (require 'core-packages (concat core-dir "core-packages"))
 (require 'core-os (concat core-dir "core-os"))
 (require 'core-editor (concat core-dir "core-editor"))
 (require 'core-ui (concat core-dir "core-ui"))
 
-;; Reduce the frequency of garbage collection by making it happen on
-;; each 25MB of allocated data (the default is on every 0.76MB)
-(setq gc-cons-threshold 25000000)
-(setq gc-cons-percentage 0.6)
+;; ;; Reduce the frequency of garbage collection by making it happen on
+;; ;; each 25MB of allocated data (the default is on every 0.76MB)
+;; (setq gc-cons-threshold 25000000)
+;; (setq gc-cons-percentage 0.6)
 
 (provide 'core)
 ;;; core.el ends here
--- init.el
+++ init.el
@@ -46,6 +46,34 @@
 (message "Your Emacs is powering up... Be patient, Master %s!" current-user)
 
 ;;----------------------------------------------------------------------------
+;; Core
+;;----------------------------------------------------------------------------
+
+;; set encoding
+(prefer-coding-system 'utf-8)
+
+;; and tell emacs to play nice with encoding
+(define-coding-system-alias 'UTF-8 'utf-8)
+(define-coding-system-alias 'utf8 'utf-8)
+
+;; save nothing
+(setq auto-save-default nil
+      create-lockfiles nil)
+
+;; no splash screen
+(setq inhibit-splash-screen t)
+
+;; no message on startup
+(setq initial-scratch-message nil)
+
+(require 'cl-lib)
+
+;; Reduce the frequency of garbage collection by making it happen on
+;; each 25MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 25000000)
+(setq gc-cons-percentage 0.6)
+
+;;----------------------------------------------------------------------------
 ;; Packages
 ;;----------------------------------------------------------------------------
 
