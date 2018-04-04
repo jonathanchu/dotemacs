e07b1ae0b231e2b6e4d22021dd5188846bfd109b
Jonathan Chu <me@jonathanchu.is>
2018-04-04 11:43:57 -0400

    Comment this out, it was getting annoying when typing "uuid"

--- init.el
+++ init.el
@@ -775,7 +775,8 @@
     (key-chord-define-global "jj" 'avy-goto-word-1)
     (key-chord-define-global "jl" 'avy-goto-line)
     (key-chord-define-global "jk" 'avy-goto-char)
-    (key-chord-define-global "uu" 'undo-tree-visualize)))
+    ;; (key-chord-define-global "uu" 'undo-tree-visualize)
+    ))
 
 (use-package latex-preview-pane
   :ensure t
