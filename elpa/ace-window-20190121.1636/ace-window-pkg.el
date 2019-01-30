5631ad29e5c7931f131e293faa1f982770f5acc9
Jonathan Chu <me@jonathanchu.is>
2019-01-29 23:14:21 -0500

    Update ace-window

--- elpa/ace-window-20190121.1636/ace-window-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ace-window" "20190121.1636" "Quickly switch windows." '((avy "0.2.0")) :stars '(#("379" 0 3 (font-lock-face paradox-star-face))) :commit "bd4802756cead0aeb3827fdd4330b0e77c96bf83" :keywords '("window" "location") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/ace-window")
---
+++ elpa/ace-window-20190129.1645/ace-window-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ace-window" "20190129.1645" "Quickly switch windows." '((avy "0.2.0")) :stars '(#("379" 0 3 (font-lock-face paradox-star-face))) :commit "88e48db56a176363121058b15b965324dc8e92c9" :keywords '("window" "location") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/ace-window")
