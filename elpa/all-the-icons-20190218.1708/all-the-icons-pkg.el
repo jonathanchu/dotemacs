519f41ac6d5d011203ee156ec5f9286d37ea03f0
Jonathan Chu <me@jonathanchu.is>
2019-02-19 13:02:26 -0500

    Update all-the-icons-20190215.101 -> all-the-icons-20190218.1708

--- elpa/all-the-icons-20190218.1708/all-the-icons-pkg.el
+++ elpa/all-the-icons-20190218.1708/all-the-icons-pkg.el
@@ -1,4 +1,4 @@
-(define-package "all-the-icons" "20190215.101" "A library for inserting Developer icons"
+(define-package "all-the-icons" "20190218.1708" "A library for inserting Developer icons"
   '((emacs "24.3")
     (memoize "1.0.1"))
   :keywords
