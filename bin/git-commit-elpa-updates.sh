caf096bcf1916a95ae6bddcddefeefdd0c21fc80
Jonathan Chu <me@jonathanchu.is>
2019-02-23 21:08:38 -0500

    Change package commit script to batch it all together

--- bin/git-commit-elpa-updates.sh
+++ bin/git-commit-elpa-updates.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 set -e
 
-ls_files () {
+ls_files_untracked () {
     root="$(git rev-parse --show-toplevel)"
     git ls-files $root --exclude-standard --others
 }
 
-ls_files1 () {
+ls_files_deleted () {
     root="$(git rev-parse --show-toplevel)"
     git ls-files $root -d
 }
 
-PACKAGES=$(ls_files | grep -Eo 'elpa/[^/]+/' | sort -u | sed 's/\(.*\)-.*/\1/')
+ls_files_modified () {
+    root="$(git rev-parse --show-toplevel)"
+    git ls-files $root -m
+}
+
+PACKAGES=$(ls_files_untracked | grep -Eo 'elpa/[^/]+/' | sort -u | sed 's/\(.*\)-.*/\1/')
+COMMIT_MSG=$'Update packages\n'
 
 for i in $PACKAGES; do
-    UNTRACKED_PACKAGE_SHORT=$(ls_files | grep -Eo 'elpa/[^/]+/' | cut -c 6- | rev | cut -c 2- | rev | sort -u | head -n 1 | sed 's/\(.*\)-.*/\1/')
-    UNTRACKED_PACKAGE_LONG=$(ls_files | grep -Eo 'elpa/[^/]+/' | cut -c 6- | rev | cut -c 2- | rev | sort -u | head -n 1)
-    UNTRACKED_PACKAGE_DIRECTORY=$(ls_files | grep -Ee "$i")
+    UNTRACKED_PACKAGE_SHORT=$(ls_files_untracked | grep -Eo 'elpa/[^/]+/' | cut -c 6- | rev | cut -c 2- | rev | sort -u | head -n 1 | sed 's/\(.*\)-.*/\1/')
+    UNTRACKED_PACKAGE_LONG=$(ls_files_untracked | grep -Eo 'elpa/[^/]+/' | cut -c 6- | rev | cut -c 2- | rev | sort -u | head -n 1)
+    UNTRACKED_PACKAGE_DIRECTORY=$(ls_files_untracked | grep -Ee "$i")
 
-    DELETED_PACKAGE_SHORT=$(ls_files1 | grep -Eo 'elpa/[^/]+/' | cut -c 6- | rev | cut -c 2- | rev | sort -u | head -n 1 | sed 's/\(.*\)-.*/\1/')
-    DELETED_PACKAGE_LONG=$(ls_files1 | grep -Eo 'elpa/[^/]+/' | cut -c 6- | rev | cut -c 2- | rev | sort -u | head -n 1)
-    DELETED_PACKAGE_DIRECTORY=$(ls_files1 | grep -Ee "$i")
+    DELETED_PACKAGE_SHORT=$(ls_files_deleted | grep -Eo 'elpa/[^/]+/' | cut -c 6- | rev | cut -c 2- | rev | sort -u | head -n 1 | sed 's/\(.*\)-.*/\1/')
+    DELETED_PACKAGE_LONG=$(ls_files_deleted | grep -Eo 'elpa/[^/]+/' | cut -c 6- | rev | cut -c 2- | rev | sort -u | head -n 1)
+    DELETED_PACKAGE_DIRECTORY=$(ls_files_deleted | grep -Ee "$i")
 
     if [ "$UNTRACKED_PACKAGE_SHORT" = "$DELETED_PACKAGE_SHORT" ]; then
         for x in $UNTRACKED_PACKAGE_DIRECTORY; do
@@ -35,8 +41,12 @@ for i in $PACKAGES; do
         done
 
         # echo "git commit -m 'Update "$DELETED_PACKAGE_LONG" -> "$UNTRACKED_PACKAGE_LONG"'"
-        git commit -m "Update "$DELETED_PACKAGE_LONG" => "$UNTRACKED_PACKAGE_LONG""
+        # git commit -m "Update "$DELETED_PACKAGE_LONG" => "$UNTRACKED_PACKAGE_LONG""
+        COMMIT_MSG="$COMMIT_MSG"$'\nUpdate '$""$DELETED_PACKAGE_LONG" => "$UNTRACKED_PACKAGE_LONG""
+        # echo $COMMIT_MSG
     else
-        echo "No action taken"
+        echo "No action taken."
     fi
 done
+
+git commit -m "$COMMIT_MSG"
