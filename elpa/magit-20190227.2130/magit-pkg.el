7ab3db0fe63a3423bbacba857ff18ffb080caea2
Jonathan Chu <me@jonathanchu.is>
2019-02-28 02:02:18 -0500

    Update magit-20190224.1537 => magit-20190227.2130

--- elpa/magit-20190227.2130/magit-pkg.el
+++ elpa/magit-20190227.2130/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20190224.1537" "A Git porcelain inside Emacs."
+(define-package "magit" "20190227.2130" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20180527")
     (dash "20180910")
