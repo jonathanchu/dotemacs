dca16c95dc99b64c631c5cf464a0259a958aaeb8
Jonathan Chu <me@jonathanchu.is>
2019-02-28 02:02:18 -0500

    Update powerline-20190224.124 => powerline-20190225.2215

--- elpa/powerline-20190225.2215/powerline-pkg.el
+++ elpa/powerline-20190225.2215/powerline-pkg.el
@@ -1,4 +1,4 @@
-(define-package "powerline" "20190224.124" "Rewrite of Powerline"
+(define-package "powerline" "20190225.2215" "Rewrite of Powerline"
   '((cl-lib "0.2"))
   :keywords
   '("mode-line")
