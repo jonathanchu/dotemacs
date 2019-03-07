5effa020a173d4e381c54f5192a82e28462194c0
Jonathan Chu <me@jonathanchu.is>
2019-03-07 09:14:17 -0500

    Update 9 packages
    Update company-lsp-20190301.2002 => company-lsp-20190306.1647
    Update counsel-20190217.1500 => counsel-20190306.1018
    Update doom-themes-20190302.1733 => doom-themes-20190307.356
    Update dumb-jump-20190221.1723 => dumb-jump-20190307.511
    Update helm-20190218.1244 => helm-20190305.1832
    Update indium-20190302.2232 => indium-20190305.2108
    Update ivy-20190214.1032 => ivy-20190306.1011
    Update magit-20190304.2230 => magit-20190307.1247
    Update which-key-20190301.1541 => which-key-20190306.1518

---
+++ elpa/dumb-jump-20190307.511/dumb-jump-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dumb-jump" "20190307.511" "jump to definition for multiple languages without configuration." '((emacs "24.3") (f "0.20.0") (s "1.11.0") (dash "2.9.0") (popup "0.5.3")) :stars '(#("709" 0 3 (font-lock-face paradox-star-face))) :commit "fc4b472699114c24b29074e1e9509eb46db4975e" :keywords '("programming") :authors '(("jack angers")) :maintainer '("jack angers"))
