1bb69662fd54f04ecf9b689b26914dcd6ae6da74
Jonathan Chu <me@jonathanchu.is>
2019-03-14 16:22:14 -0400

    Update 13 packages
    Update all-the-icons-20190223.2143 => all-the-icons-20190313.1420
    Update clojure-mode-20190302.1102 => clojure-mode-20190313.1836
    Update company-lsp-20190306.1647 => company-lsp-20190314.710
    Update counsel-20190308.1042 => counsel-20190314.1047
    Update doom-themes-20190307.356 => doom-themes-20190313.2220
    Update ghub-20190311.1904 => ghub-20190313.1505
    Update git-timemachine-20181204.1546 => git-timemachine-20190314.1153
    Update helm-20190312.614 => helm-20190313.1509
    Update lsp-mode-20190310.2011 => lsp-mode-20190314.1656
    Update lsp-ui-20190311.948 => lsp-ui-20190314.1008
    Update magit-20190311.1451 => magit-20190314.1221
    Update python-mode-20181223.1933 => python-mode-20190313.1109
    Update scratch-20170614.2101 => scratch-20190314.614

--- elpa/clojure-mode-20190302.1102/clojure-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "clojure-mode" "20190302.1102" "Major mode for Clojure code" '((emacs "25.1")) :stars '(#("657" 0 3 (font-lock-face paradox-star-face))) :commit "a1d6a0f1b53dc454333be99eae1517315267aa6b" :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
--- elpa/counsel-20190308.1042/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20190308.1042" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.11.0")) :stars '(#("923" 0 3 (font-lock-face paradox-star-face))) :commit "a538df11747e98ec2af0635558d04082211a15bf" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
