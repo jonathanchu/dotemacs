1b879d33ba32f2f39f112aec9bd12c647523b17e
Jonathan Chu <me@jonathanchu.is>
2019-03-20 00:21:21 -0400

    Update 9 packages
    Update avy-20190204.1201 => avy-20190319.1813
    Update counsel-20190314.1047 => counsel-20190319.1757
    Update expand-region-20190312.828 => expand-region-20190319.2021
    Update flycheck-20190311.1422 => flycheck-20190319.2312
    Update ghub-20190313.1505 => ghub-20190319.2343
    Update git-commit-20190102.107 => git-commit-20190319.2352
    Update magit-20190319.1356 => magit-20190319.1911
    Update paradox-20190303.27 => paradox-20190319.1439
    Update transient-20190319.10 => transient-20190319.1928

---
+++ elpa/avy-20190319.1813/avy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "avy" "20190319.1813" "Jump to arbitrary positions in visible text and select text quickly." '((emacs "24.1") (cl-lib "0.5")) :stars '("") :commit "7d73ceeb74d5fd912fe48e729300acae04bbf659" :keywords '("point" "location") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/avy")
---
+++ elpa/git-commit-20190319.2352/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190319.2352" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :stars '("") :commit "24fd932a2344306be82efec599212b0535abf253" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
