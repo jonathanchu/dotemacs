f909533316a271487f61a2aef109f942af27bf91
Jonathan Chu <me@jonathanchu.is>
2019-03-23 14:08:30 -0400

    Remove package nov.el
    Not working out ¯\_(ツ)_/¯

--- init.el
+++ init.el
@@ -991,11 +991,6 @@
 (use-package noflet
   :ensure t)
 
-(use-package nov
-  :ensure t
-  :config
-  (add-to-list 'auto-mode-alist '("\\.epub\\" . nov-mode)))
-
 (use-package olivetti
   :ensure t)
 
