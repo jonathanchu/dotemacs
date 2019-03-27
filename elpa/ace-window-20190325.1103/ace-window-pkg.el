cdcacf4adcab8c29c8b897bae0c301518f47e52f
Jonathan Chu <me@jonathanchu.is>
2019-03-27 14:23:54 -0400

    Update 6 packages
    Update hydra-20190213.1849 => hydra-20190326.1950
    Update ivy-20190321.935 => ivy-20190327.1154
    Update lsp-mode-20190325.1333 => lsp-mode-20190326.522
    Update magit-20190323.2151 => magit-20190326.2143
    Update nlinum-1.8.1 => nlinum-1.9
    Update projectile-20190323.904 => projectile-20190326.759

--- elpa/ace-window-20190325.1103/ace-window-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ace-window" "20190325.1103" "Quickly switch windows." '((avy "0.2.0")) :stars '(#("451" 0 3 (font-lock-face paradox-star-face))) :commit "2d132bab253de7c09500881b4302f47d15584253" :keywords '("window" "location") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/ace-window")
--- elpa/expand-region-20190322.712/expand-region-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "expand-region" "20190322.712" "Increase selected region by semantic units." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
