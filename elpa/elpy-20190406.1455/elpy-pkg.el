10e559da2588f636f4776b9f36ba2f4b23f3fb1c
Jonathan Chu <me@jonathanchu.is>
2019-04-06 13:32:39 -0400

    Update 1 package
    Update elpy-20190405.1631 => elpy-20190406.1455

--- elpa/elpy-20190406.1455/elpy-pkg.el
+++ elpa/elpy-20190406.1455/elpy-pkg.el
@@ -1,4 +1,4 @@
-(define-package "elpy" "20190405.1631" "Emacs Python Development Environment"
+(define-package "elpy" "20190406.1455" "Emacs Python Development Environment"
   '((company "0.9.2")
     (emacs "24.4")
     (find-file-in-project "3.3")
