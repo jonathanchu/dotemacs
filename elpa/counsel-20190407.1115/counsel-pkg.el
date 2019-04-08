cbba686df9176407499eb16cd51ceb73e9be4341
Jonathan Chu <me@jonathanchu.is>
2019-04-08 16:43:01 -0400

    Update 5 packages
    Update hydra-20190405.1015 => hydra-20190408.1006
    Update ivy-20190405.1833 => ivy-20190408.1052
    Update lsp-mode-20190407.739 => lsp-mode-20190408.1022
    Update magit-20190405.1704 => magit-20190408.1739
    Update swiper-20190407.1501 => swiper-20190407.1729

--- elpa/counsel-20190407.1115/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20190407.1115" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.11.0")) :stars '(#("1136" 0 4 (font-lock-face paradox-star-face))) :commit "11b00073cb348aedf1d648a991bab71e20178268" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
---
+++ elpa/swiper-20190407.1729/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20190407.1729" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.11.0")) :stars '(#("1136" 0 4 (font-lock-face paradox-star-face))) :commit "92ec03c3f10809a0bd790bca41c221215237226e" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
