b9908cb2d01c8ba7ab5a14a63dc59d177268f6e6
Jonathan Chu <me@jonathanchu.is>
2019-04-22 10:14:25 -0400

    Update 9 packages
    Update company-lsp-20190326.553 => company-lsp-20190420.609
    Update flycheck-20190419.1418 => flycheck-20190420.2021
    Update helm-20190419.2050 => helm-20190421.1727
    Update helm-core-20190418.1747 => helm-core-20190422.835
    Update js2-mode-20190307.1649 => js2-mode-20190420.2244
    Update lsp-mode-20190419.843 => lsp-mode-20190422.1239
    Update lsp-ui-20190413.1606 => lsp-ui-20190420.428
    Update magit-20190419.2028 => magit-20190421.1652
    Update yasnippet-20190414.1606 => yasnippet-20190421.2011

---
+++ elpa/company-lsp-20190420.609/company-lsp-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-lsp" "20190420.609" "Company completion backend for lsp-mode." '((emacs "25.1") (lsp-mode "6.0") (company "0.9.0") (s "1.2.0") (dash "2.11.0")) :stars '(#("147" 0 3 (font-lock-face paradox-star-face))) :commit "afef816b1411c63c30cab09f957fbbcea0617742" :url "https://github.com/tigersoldier/company-lsp")
