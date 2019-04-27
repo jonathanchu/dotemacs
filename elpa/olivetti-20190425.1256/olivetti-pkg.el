39cfb6e1aeda26eb24262a61ab04bf3a1d80bf56
Jonathan Chu <me@jonathanchu.is>
2019-04-27 11:38:56 -0400

    Update 10 packages
    Update doom-themes-20190424.518 => doom-themes-20190424.2243
    Update elpy-20190416.1701 => elpy-20190425.2331
    Update exec-path-from-shell-20190106.307 => exec-path-from-shell-20190426.2227
    Update flycheck-20190420.2021 => flycheck-20190425.816
    Update git-commit-20190413.1201 => git-commit-20190426.1503
    Update helm-core-20190423.1550 => helm-core-20190426.1525
    Update lsp-mode-20190424.2016 => lsp-mode-20190427.1313
    Update magit-20190424.1159 => magit-20190426.1457
    Update olivetti-20190330.1633 => olivetti-20190425.1256
    Update transient-20190424.1117 => transient-20190425.948

---
+++ elpa/olivetti-20190425.1256/olivetti-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "olivetti" "20190425.1256" "Minor mode for a nice writing environment" '((emacs "24.5")) :stars '(#("259" 0 3 (font-lock-face paradox-star-face))) :commit "46fdcfeeee026f23d4f835890454541d3335af8f" :keywords '("wp" "text") :authors '(("Paul Rankin" . "hello@paulwrankin.com")) :maintainer '("Paul Rankin" . "hello@paulwrankin.com"))
