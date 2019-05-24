0d7024485d4e1ba1dd13914e789f68874d1c6bd4
Jonathan Chu <me@jonathanchu.is>
2019-05-24 12:18:17 -0400

    Update 16 packages
    Update ace-window-20190514.830 => ace-window-20190522.1544
    Update counsel-20190519.1216 => counsel-20190523.2008
    Update doom-themes-20190520.1646 => doom-themes-20190521.419
    Update elpy-20190508.1521 => elpy-20190523.2246
    Update ghub-20190513.1333 => ghub-20190523.1635
    Update helm-20190520.1130 => helm-20190522.1501
    Update helm-core-20190520.1306 => helm-core-20190523.1035
    Update hydra-20190517.1208 => hydra-20190520.1826
    Update ivy-20190518.1814 => ivy-20190523.921
    Update lsp-mode-20190520.1604 => lsp-mode-20190524.1058
    Update lsp-ui-20190518.1842 => lsp-ui-20190523.1521
    Update magit-20190517.1750 => magit-20190524.1250
    Update smartparens-20190519.1206 => smartparens-20190522.2234
    Update swiper-20190518.2158 => swiper-20190522.1011
    Update transient-20190518.2115 => transient-20190524.907
    Update web-mode-20190506.607 => web-mode-20190522.610

--- elpa/swiper-20190518.2158/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20190518.2158" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.11.0")) :stars '(#("1168" 0 4 (font-lock-face paradox-star-face))) :commit "77a5ffc3357ed5b01e0883fa001815d1b23e2a23" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
