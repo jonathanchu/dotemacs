877eac32d91c7447c2b71600e60d67e32c5e820b
Jonathan Chu <me@jonathanchu.is>
2019-05-31 11:19:10 -0400

    Update 15 packages
    Update ace-window-20190522.1544 => ace-window-20190527.1351
    Update counsel-20190523.2008 => counsel-20190527.1406
    Update ghub-20190523.1635 => ghub-20190529.1038
    Update helm-20190522.1501 => helm-20190527.1253
    Update helm-core-20190523.1035 => helm-core-20190527.1253
    Update hydra-20190520.1826 => hydra-20190525.817
    Update ivy-20190523.921 => ivy-20190530.1924
    Update lsp-mode-20190524.1650 => lsp-mode-20190529.1958
    Update magit-20190524.1250 => magit-20190525.1118
    Update projectile-20190509.902 => projectile-20190529.515
    Update reformatter-20190517.443 => reformatter-20190529.2238
    Update swiper-20190522.1011 => swiper-20190528.1333
    Update transient-20190524.907 => transient-20190528.804
    Update use-package-20190405.2047 => use-package-20190529.2040
    Update which-key-20190518.157 => which-key-20190529.114

--- elpa/which-key-20190518.157/which-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "which-key" "20190518.157" "Display available keybindings in popup" '((emacs "24.4")) :stars '(#("731" 0 3 (font-lock-face paradox-star-face))) :commit "77586e62b6d306bae07e4e3e093ba488724f4131" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-which-key")
