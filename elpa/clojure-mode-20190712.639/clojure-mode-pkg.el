02f3730019f581f90e6037a245d354004cf0658b
Jonathan Chu <me@jonathanchu.is>
2019-07-13 02:17:14 -0400

    Update 9 packages
    Update centaur-tabs-20190710.1631 => centaur-tabs-20190713.103
    Update clojure-mode-20190710.1239 => clojure-mode-20190712.639
    Update doom-modeline-20190708.1753 => doom-modeline-20190712.1752
    Update go-mode-20190711.149 => go-mode-20190711.1528
    Update helm-20190705.558 => helm-20190712.1653
    Update helm-core-20190708.1819 => helm-core-20190712.1716
    Update lsp-mode-20190709.1140 => lsp-mode-20190711.449
    Update magit-20190709.1914 => magit-20190711.1930
    Update python-mode-20190710.940 => python-mode-20190712.1640

---
+++ elpa/clojure-mode-20190712.639/clojure-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "clojure-mode" "20190712.639" "Major mode for Clojure code" '((emacs "25.1")) :stars '(#("695" 0 3 (font-lock-face paradox-star-face))) :commit "c7171e4546f5ed38d3b07cdd8d069e97496ab777" :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
--- elpa/go-mode-20190711.149/go-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-mode" "20190711.149" "Major mode for the Go programming language" 'nil :stars '(#("910" 0 3 (font-lock-face paradox-star-face))) :commit "287595a375388cce16ecd8f44b44008e7679bc59" :keywords '("languages" "go") :authors '(("The go-mode Authors")) :maintainer '("The go-mode Authors") :url "https://github.com/dominikh/go-mode.el")
