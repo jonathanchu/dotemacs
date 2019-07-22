95ac0e25bd104a2f38453200181e80f745c3d42f
Jonathan Chu <me@jonathanchu.is>
2019-07-22 10:27:14 -0400

    Update 4 packages
    Update elpy-20190719.2354 => elpy-20190722.1329
    Update helm-20190721.1304 => helm-20190722.550
    Update ivy-20190721.1039 => ivy-20190722.1352
    Update swiper-20190721.1029 => swiper-20190722.1352

--- elpa/swiper-20190721.1029/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20190721.1029" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.12.0")) :stars '(#("1168" 0 4 (font-lock-face paradox-star-face))) :commit "039353df6b7021c8f568dc55dafe4e1fb8bf3807" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
