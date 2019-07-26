023aa979095a4b33d94c18c9b6d1664232e4a85c
Jonathan Chu <me@jonathanchu.is>
2019-07-26 01:04:52 -0400

    Update moar packages ¯\_(ツ)_/¯

--- elpa/clojure-mode-20190725.654/clojure-mode-autoloads.el
+++ elpa/clojure-mode-20190725.654/clojure-mode-autoloads.el
@@ -152,6 +152,10 @@ Major mode for editing ClojureC code.
 
 ;;;***
 
+;;;### (autoloads nil nil ("clojure-mode-pkg.el") (0 0 0 0))
+
+;;;***
+
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
--- elpa/counsel-20190724.2102/counsel-autoloads.el
+++ elpa/counsel-20190724.2102/counsel-autoloads.el
@@ -573,6 +573,10 @@ Local bindings (`counsel-mode-map'):
 
 ;;;***
 
+;;;### (autoloads nil nil ("counsel-pkg.el") (0 0 0 0))
+
+;;;***
+
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
