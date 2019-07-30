611fd5c14e1c5156bfda78157c5eb1870eaf472e
Jonathan Chu <me@jonathanchu.is>
2019-07-30 09:48:57 -0400

    Remove default setting for use-package deferred loading

--- init.el
+++ init.el
@@ -100,8 +100,7 @@
   (package-install 'use-package))
 
 ;; Bootstrap `use-package'
-(setq-default use-package-always-defer t ; Always defer load package to speed up startup time
-              use-package-verbose nil ; Don't report loading details
+(setq-default use-package-verbose nil ; Don't report loading details
               use-package-expand-minimally t  ; make the expanded code as minimal as possible
               use-package-enable-imenu-support t) ; Let imenu finds use-package definitions
 (eval-when-compile
