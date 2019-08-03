2bbbce28b271c5143c3a9d3fdf7c6db666008fbc
Jonathan Chu <me@jonathanchu.is>
2019-08-03 14:25:08 -0400

    Clean up unused and outdated packages

--- elpa/go-mode-20190731.53/go-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-mode" "20190731.53" "Major mode for the Go programming language" 'nil :stars '(#("910" 0 3 (font-lock-face paradox-star-face))) :commit "59dfa9627d41d82b0261fabb74432ed908db4e0b" :keywords '("languages" "go") :authors '(("The go-mode Authors")) :maintainer '("The go-mode Authors") :url "https://github.com/dominikh/go-mode.el")
