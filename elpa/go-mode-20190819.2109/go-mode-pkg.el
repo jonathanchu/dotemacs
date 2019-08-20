323b2f0a5fcd7971d65f7d5d966f2e23120af229
Jonathan Chu <me@jonathanchu.is>
2019-08-20 11:45:05 -0400

    Update 10 packages
    Update doom-modeline-20190816.1456 => doom-modeline-20190819.1829
    Update doom-themes-20190814.1614 => doom-themes-20190819.1549
    Update go-mode-20190815.2113 => go-mode-20190819.2109
    Update helm-20190816.918 => helm-20190819.824
    Update helm-core-20190809.1008 => helm-core-20190820.1142
    Update lsp-mode-20190815.1531 => lsp-mode-20190819.520
    Update magit-20190816.933 => magit-20190819.1414
    Update multiple-cursors-20190317.1211 => multiple-cursors-20190820.749
    Update org-journal-20190816.1412 => org-journal-20190816.2036
    Update python-mode-20190724.633 => python-mode-20190819.1244

---
+++ elpa/go-mode-20190819.2109/go-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-mode" "20190819.2109" "Major mode for the Go programming language" 'nil :stars '(#("910" 0 3 (font-lock-face paradox-star-face))) :commit "9ab06b3deb1cbf00802d7824bf7107c31865f9fb" :keywords '("languages" "go") :authors '(("The go-mode Authors")) :maintainer '("The go-mode Authors") :url "https://github.com/dominikh/go-mode.el")
