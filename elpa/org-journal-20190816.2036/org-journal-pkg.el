57dfedf9752001c4d89b7c2b21255d2012322eb9
Jonathan Chu <me@jonathanchu.is>
2019-08-27 16:24:24 -0400

    Update 16 packages
    Update avy-20190630.1538 => avy-20190827.1807
    Update cider-20190820.537 => cider-20190826.911
    Update company-20190430.1852 => company-20190821.658
    Update counsel-20190809.1548 => counsel-20190822.1708
    Update doom-modeline-20190819.1829 => doom-modeline-20190820.1749
    Update doom-themes-20190819.1549 => doom-themes-20190821.2117
    Update flycheck-20190807.813 => flycheck-20190821.1725
    Update helm-20190819.824 => helm-20190827.1311
    Update hydra-20190617.859 => hydra-20190821.939
    Update ivy-20190809.1551 => ivy-20190825.1023
    Update lsp-mode-20190819.520 => lsp-mode-20190827.1505
    Update lsp-ui-20190809.1907 => lsp-ui-20190823.541
    Update lv-20190716.1741 => lv-20190821.947
    Update magit-20190819.1414 => magit-20190825.1416
    Update org-journal-20190816.2036 => org-journal-20190826.1919
    Update swiper-20190801.1110 => swiper-20190822.1708

--- elpa/org-journal-20190816.2036/org-journal-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-journal" "20190816.2036" "a simple org-mode based journaling mode" '((emacs "25.1")) :stars '(#("443" 0 3 (font-lock-face paradox-star-face))) :commit "eb5e6cc56bfef96997b3477403bbc30f320f59fa" :authors '(("Bastian Bechtold")) :maintainer '("Bastian Bechtold") :url "http://github.com/bastibe/org-journal")
