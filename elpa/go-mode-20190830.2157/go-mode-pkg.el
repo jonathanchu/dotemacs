9663619be566cc16838f842a769c09e1fcf00b98
Jonathan Chu <me@jonathanchu.is>
2019-09-09 11:30:46 -0400

    Update 10 packages
    Update company-20190904.1002 => company-20190907.1149
    Update doom-modeline-20190904.2007 => doom-modeline-20190909.918
    Update dumb-jump-20190831.401 => dumb-jump-20190907.450
    Update elpy-20190906.2005 => elpy-20190908.1527
    Update go-mode-20190830.2157 => go-mode-20190908.2239
    Update lsp-mode-20190906.1727 => lsp-mode-20190908.1722
    Update magit-20190906.217 => magit-20190906.2217
    Update olivetti-20190519.1316 => olivetti-20190907.2028
    Update python-mode-20190906.839 => python-mode-20190908.1955
    Update sesman-20190623.1123 => sesman-20190908.1138

--- elpa/go-mode-20190830.2157/go-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-mode" "20190830.2157" "Major mode for the Go programming language" 'nil :stars '(#("910" 0 3 (font-lock-face paradox-star-face))) :commit "a13b8146bfde207513327d95e5e85cefadf99832" :keywords '("languages" "go") :authors '(("The go-mode Authors")) :maintainer '("The go-mode Authors") :url "https://github.com/dominikh/go-mode.el")
---
+++ elpa/sesman-20190908.1138/sesman-pkg.el
@@ -0,0 +1,12 @@
+(define-package "sesman" "20190908.1138" "Generic Session Manager"
+  '((emacs "25"))
+  :keywords
+  '("process")
+  :authors
+  '(("Vitalie Spinu"))
+  :maintainer
+  '("Vitalie Spinu")
+  :url "https://github.com/vspinu/sesman")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
