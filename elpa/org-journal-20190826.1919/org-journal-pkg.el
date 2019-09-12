3e39f0392221ebb240dc74aaca5371ef7ed3b80a
Jonathan Chu <me@jonathanchu.is>
2019-09-12 17:06:53 -0500

    Update 9 packages
    Update cider-20190903.1609 => cider-20190911.1917
    Update dumb-jump-20190907.450 => dumb-jump-20190912.531
    Update elpy-20190908.1527 => elpy-20190912.1502
    Update lsp-mode-20190908.1722 => lsp-mode-20190912.444
    Update lsp-ui-20190823.541 => lsp-ui-20190912.946
    Update org-journal-20190826.1919 => org-journal-20190910.1834
    Update python-mode-20190908.1955 => python-mode-20190912.1653
    Update sesman-20190908.1138 => sesman-20190909.1754
    Update smartparens-20190902.1105 => smartparens-20190904.1742

--- elpa/org-journal-20190826.1919/org-journal-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-journal" "20190826.1919" "a simple org-mode based journaling mode" '((emacs "25.1")) :stars '(#("443" 0 3 (font-lock-face paradox-star-face))) :commit "cb15adcec09a891911bd2a85cbbfd45502e65f00" :authors '(("Bastian Bechtold")) :maintainer '("Bastian Bechtold") :url "http://github.com/bastibe/org-journal")
---
+++ elpa/org-journal-20190910.1834/org-journal-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-journal" "20190910.1834" "a simple org-mode based journaling mode" '((emacs "25.1")) :stars '(#("443" 0 3 (font-lock-face paradox-star-face))) :commit "98e2de79c947740e4e6cdfe5338adb4819233680" :authors '(("Bastian Bechtold")) :maintainer '("Bastian Bechtold") :url "http://github.com/bastibe/org-journal")
