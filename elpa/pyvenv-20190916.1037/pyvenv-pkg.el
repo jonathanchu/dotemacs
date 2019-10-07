c2e86c7d33f48ad61dd8ce8b6ae672c0915acb79
Jonathan Chu <me@jonathanchu.is>
2019-10-06 20:39:51 -0400

    Update 11 packages
    Update cider-20191003.537 => cider-20191006.1936
    Update counsel-20191003.1236 => counsel-20191006.1302
    Update doom-themes-20191004.2113 => doom-themes-20191005.2009
    Update ghub-20191004.2206 => ghub-20191005.1236
    Update helm-20191003.727 => helm-20191006.1608
    Update ivy-20190919.1331 => ivy-20191005.1556
    Update lsp-mode-20191004.1532 => lsp-mode-20191006.1704
    Update projectile-20190904.1025 => projectile-20191006.926
    Update pyvenv-20190916.1037 => pyvenv-20191006.1304
    Update reformatter-20190529.2238 => reformatter-20191006.2321
    Update swiper-20190822.1708 => swiper-20191005.1554

--- elpa/pyvenv-20190916.1037/pyvenv-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pyvenv" "20190916.1037" "Python virtual environment interface" 'nil :stars '(#("170" 0 3 (font-lock-face paradox-star-face))) :commit "392e28dad42dc6cc9507e496391a32482f9f1881" :keywords '("python" "virtualenv" "tools") :authors '(("Jorgen Schaefer" . "contact@jorgenschaefer.de")) :maintainer '("Jorgen Schaefer" . "contact@jorgenschaefer.de") :url "http://github.com/jorgenschaefer/pyvenv")
