9a2ee41eca9fed5d8dd44034b2454b8978a7a56f
Jonathan Chu <me@jonathanchu.is>
2019-10-24 02:17:48 -0400

    Update 14 packages
    Update ace-window-20190708.933 => ace-window-20191022.1203
    Update centaur-tabs-20191016.1451 => centaur-tabs-20191020.237
    Update counsel-20191017.1812 => counsel-20191022.1207
    Update doom-modeline-20191019.159 => doom-modeline-20191023.531
    Update doom-themes-20191011.1514 => doom-themes-20191023.1600
    Update elpy-20191017.2348 => elpy-20191022.2056
    Update flycheck-20190930.1425 => flycheck-20191022.1117
    Update go-mode-20191018.2048 => go-mode-20191022.2037
    Update helm-20191010.1701 => helm-20191023.1610
    Update indium-20190925.1909 => indium-20191022.2128
    Update ivy-20191018.1251 => ivy-20191021.1017
    Update lsp-ui-20191016.1644 => lsp-ui-20191023.1558
    Update projectile-20191006.926 => projectile-20191021.1201
    Update swiper-20191016.1657 => swiper-20191021.1015

--- elpa/swiper-20191016.1657/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20191016.1657" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :stars '(#("1168" 0 4 (font-lock-face paradox-star-face))) :commit "cd634c6f51458f81898ecf2821ac3169cb65a1eb" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
