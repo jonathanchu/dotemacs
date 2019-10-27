08546ebdc2c7d896911f6083ecc269e0b51704a8
Jonathan Chu <me@jonathanchu.is>
2019-10-27 10:47:49 -0400

    Update 16 packages
    Update all-the-icons-20190320.1809 => all-the-icons-20191025.43
    Update avy-20190925.1054 => avy-20191024.2310
    Update cider-20191019.1042 => cider-20191025.1511
    Update counsel-20191022.1207 => counsel-20191026.922
    Update dash-20190920.1035 => dash-20191024.1908
    Update doom-modeline-20191023.531 => doom-modeline-20191027.821
    Update doom-themes-20191023.1600 => doom-themes-20191026.102
    Update elpy-20191022.2056 => elpy-20191024.2007
    Update helm-20191023.1610 => helm-20191026.431
    Update lsp-mode-20191019.1116 => lsp-mode-20191026.1912
    Update lv-20191016.1443 => lv-20191025.1326
    Update magit-20191017.1800 => magit-20191022.1848
    Update ox-hugo-20190905.303 => ox-hugo-20191026.2105
    Update projectile-20191021.1201 => projectile-20191024.721
    Update python-mode-20191018.1735 => python-mode-20191025.1919
    Update with-editor-20191008.2002 => with-editor-20191024.1905

--- elpa/dash-20190920.1035/dash-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dash" "20190920.1035" "A modern list library for Emacs" 'nil :stars '(#("969" 0 3 (font-lock-face paradox-star-face))) :commit "a743ae3da1d5869434c6f262bbe45ef30d87cb9c" :keywords '("lists") :authors '(("Magnar Sveen" . "magnars@gmail.com")) :maintainer '("Magnar Sveen" . "magnars@gmail.com"))
