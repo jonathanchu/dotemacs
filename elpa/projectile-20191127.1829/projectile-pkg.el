818619d181697fdb7aaa7f95d20c5aaffa6c5a27
Jonathan Chu <me@jonathanchu.is>
2019-12-01 00:28:47 -0500

    Update 10 packages
    Update doom-modeline-20191129.1524 => doom-modeline-20191130.1500
    Update forge-20191123.1448 => forge-20191130.1401
    Update helm-core-20191128.1814 => helm-core-20191130.2010
    Update magit-20191123.1420 => magit-20191128.1802
    Update org-super-agenda-20190925.958 => org-super-agenda-20191130.2245
    Update ox-hugo-20191026.2105 => ox-hugo-20191130.512
    Update projectile-20191024.721 => projectile-20191127.1829
    Update swiper-20191114.1539 => swiper-20191128.1126
    Update which-key-20191111.1951 => which-key-20191127.1700
    Update yaml-mode-20190625.1740 => yaml-mode-20191127.2314

---
+++ elpa/projectile-20191127.1829/projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "projectile" "20191127.1829" "Manage and navigate projects in Emacs easily" '((emacs "25.1") (pkg-info "0.4")) :stars '(#("2614" 0 4 (font-lock-face paradox-star-face))) :commit "520767bd5c21e172fd95bb42c5a7631b07f2ff9a" :keywords '("project" "convenience") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "https://github.com/bbatsov/projectile")
--- elpa/swiper-20191114.1539/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20191114.1539" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :stars '(#("1168" 0 4 (font-lock-face paradox-star-face))) :commit "abfb28311e97c1dcc2e8d7f6c21701d4344bdd6b" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
