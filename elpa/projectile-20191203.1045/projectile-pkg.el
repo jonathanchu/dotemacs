e2c1dbbea4987312c678184bbe2ec9ff59ead251
Jonathan Chu <me@jonathanchu.is>
2019-12-10 11:52:31 -0500

    Cleanup old packages

--- elpa/projectile-20191203.1045/projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "projectile" "20191203.1045" "Manage and navigate projects in Emacs easily" '((emacs "25.1") (pkg-info "0.4")) :stars '(#("2614" 0 4 (font-lock-face paradox-star-face))) :commit "7f4f29371dfcb3788fb1dddba87046a6fc6545c8" :keywords '("project" "convenience") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "https://github.com/bbatsov/projectile")
