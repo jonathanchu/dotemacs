d31535802fff872b7cb480680c968f0bb6e35f2f
Jonathan Chu <me@jonathanchu.is>
2019-12-14 17:00:14 -0500

    Update 14 packages
    Update cider-20191211.1133 => cider-20191212.2000
    Update company-20191206.33 => company-20191213.2320
    Update counsel-20191127.1747 => counsel-20191214.1418
    Update flow-minor-mode-20180315.1824 => flow-minor-mode-20191214.1309
    Update forge-20191211.1610 => forge-20191212.2140
    Update ghub-20191205.1648 => ghub-20191212.2202
    Update helm-20191211.1514 => helm-20191214.825
    Update helm-core-20191211.1514 => helm-core-20191214.825
    Update ivy-20191128.1153 => ivy-20191214.1423
    Update lsp-mode-20191211.1952 => lsp-mode-20191213.1621
    Update lsp-ui-20191209.1110 => lsp-ui-20191214.1645
    Update lv-20191106.1238 => lv-20191214.1357
    Update magit-20191210.1617 => magit-20191214.131
    Update yasnippet-20191030.1331 => yasnippet-20191213.2101

---
+++ elpa/counsel-20191214.1418/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20191214.1418" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :stars '(#("1168" 0 4 (font-lock-face paradox-star-face))) :commit "2d4d6d690702f943b4f71e0d2a6604d1eb6ef646" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
---
+++ elpa/flow-minor-mode-20191214.1309/flow-minor-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flow-minor-mode" "20191214.1309" "Flow type mode based on web-mode." '((emacs "25.1")) :stars '(#("36" 0 2 (font-lock-face paradox-star-face))) :commit "5db3936d9eba8ccb2beca476afc84675b7b161ca" :url "https://github.com/an-sh/flow-minor-mode")
