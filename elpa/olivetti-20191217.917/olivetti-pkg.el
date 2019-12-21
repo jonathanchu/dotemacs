0b94bd599dba441fa3b07fe9bac997e5b26e6f17
Jonathan Chu <me@jonathanchu.is>
2019-12-21 12:53:15 -0500

    Update 16 packages
    Update async-20191030.2138 => async-20191220.932
    Update clj-refactor-20190618.716 => clj-refactor-20191217.909
    Update company-20191213.2320 => company-20191221.1324
    Update counsel-20191214.1418 => counsel-20191219.1246
    Update evil-20191211.1315 => evil-20191221.1611
    Update forge-20191212.2140 => forge-20191218.1328
    Update go-mode-20191210.1823 => go-mode-20191221.9
    Update helm-20191214.825 => helm-20191221.801
    Update helm-core-20191214.825 => helm-core-20191218.1618
    Update ivy-20191214.1423 => ivy-20191217.1242
    Update lsp-mode-20191216.606 => lsp-mode-20191221.1518
    Update magit-20191215.1509 => magit-20191220.2218
    Update olivetti-20190923.840 => olivetti-20191217.917
    Update ox-hugo-20191204.1552 => ox-hugo-20191217.1543
    Update projectile-20191204.1451 => projectile-20191216.2105
    Update python-mode-20191201.1223 => python-mode-20191220.1543

---
+++ elpa/olivetti-20191217.917/olivetti-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "olivetti" "20191217.917" "Minor mode for a nice writing environment" '((emacs "24.5")) :stars '(#("275" 0 3 (font-lock-face paradox-star-face))) :commit "6c7505f426c1bb55bb209e45ea69e9a3cd683be0" :keywords '("wp" "text") :authors '(("Paul W. Rankin" . "code@paulwrankin.com")) :maintainer '("Paul W. Rankin" . "code@paulwrankin.com") :url "https://gthub.com/rnkn/olivetti")
