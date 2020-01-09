77aec8d07942fc97373e7937feb7e3deebcf3995
Jonathan Chu <me@jonathanchu.is>
2020-01-09 11:05:24 -0500

    Update 15 packages
    Update counsel-20200105.1947 => counsel-20200108.1559
    Update doom-modeline-20200106.433 => doom-modeline-20200106.622
    Update expand-region-20190416.538 => expand-region-20200106.2058
    Update ghub-20200102.2204 => ghub-20200108.1233
    Update go-mode-20191221.9 => go-mode-20200107.546
    Update helm-20200105.1648 => helm-20200107.1518
    Update helm-core-20191223.1914 => helm-core-20200108.724
    Update ivy-20200105.1946 => ivy-20200108.1559
    Update lsp-mode-20200105.1812 => lsp-mode-20200109.703
    Update lsp-ui-20200105.1808 => lsp-ui-20200106.1152
    Update magit-20200102.2204 => magit-20200109.516
    Update org-ql-20200103.1257 => org-ql-20200108.1858
    Update org-super-agenda-20191130.2245 => org-super-agenda-20200107.1609
    Update transient-20200102.2204 => transient-20200107.2156
    Update undo-tree-0.6.5 => undo-tree-0.7.1

--- elpa/counsel-20200105.1947/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20200105.1947" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :stars '(#("1168" 0 4 (font-lock-face paradox-star-face))) :commit "baac6e1c32ee895ea543f341a62204946237fe9c" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
---
+++ elpa/go-mode-20200107.546/go-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-mode" "20200107.546" "Major mode for the Go programming language" 'nil :stars '(#("910" 0 3 (font-lock-face paradox-star-face))) :commit "e10d6775f486ef09ed993567b0bd2c69f35deb45" :keywords '("languages" "go") :authors '(("The go-mode Authors")) :maintainer '("The go-mode Authors") :url "https://github.com/dominikh/go-mode.el")
