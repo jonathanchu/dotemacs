b5ef2dd172c5696af2822da19dd3549301a3d172
Jonathan Chu <me@jonathanchu.is>
2020-01-19 22:30:42 -0500

    Update 14 packages
    Update centaur-tabs-20200103.1548 => centaur-tabs-20200119.2059
    Update cider-20200109.1720 => cider-20200119.1234

--- elpa/dash-20191109.1327/dash-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dash" "20191109.1327" "A modern list library for Emacs" 'nil :stars '(#("969" 0 3 (font-lock-face paradox-star-face))) :commit "13e5366898df483a2bbd95a06c35e15b463ee2d1" :keywords '("lists") :authors '(("Magnar Sveen" . "magnars@gmail.com")) :maintainer '("Magnar Sveen" . "magnars@gmail.com"))
