6c27f5e9527071d197380f7523b5914d6fb524a0
Jonathan Chu <me@jonathanchu.is>
2020-02-03 12:42:10 -0500

    Update 18 packages
    Update ace-window-20200115.1506 => ace-window-20200201.1753
    Update beginend-20191030.839 => beginend-20200203.1035
    Update centaur-tabs-20200119.2059 => centaur-tabs-20200128.212
    Update counsel-20200122.1137 => counsel-20200201.1131
    Update doom-modeline-20200124.1510 => doom-modeline-20200203.524
    Update doom-themes-20200126.1648 => doom-themes-20200203.625
    Update elpy-20200112.1819 => elpy-20200202.2031
    Update esup-20200127.1709 => esup-20200130.2034
    Update helm-20200127.1826 => helm-20200128.1831
    Update ivy-20200122.1139 => ivy-20200203.1114
    Update lsp-mode-20200127.1824 => lsp-mode-20200203.538
    Update lsp-python-ms-20200114.1228 => lsp-python-ms-20200203.946
    Update lsp-ui-20200115.1352 => lsp-ui-20200130.943
    Update olivetti-20200125.439 => olivetti-20200202.1605
    Update org-super-agenda-20200118.1804 => org-super-agenda-20200129.16
    Update projectile-20200125.1343 => projectile-20200128.1155
    Update undo-tree-0.7.3 => undo-tree-0.7.4
    Update vterm-20200128.123 => vterm-20200203.1548

---
+++ elpa/beginend-20200203.1035/beginend-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "beginend" "20200203.1035" "Redefine M-< and M-> for some modes" '((emacs "25.3")) :stars '(#("61" 0 2 (font-lock-face paradox-star-face))) :commit "3ab68d77839060f5e85bbf9455846b68cc63ef00" :url "https://github.com/DamienCassou/beginend")
---
+++ elpa/lsp-python-ms-20200203.946/lsp-python-ms-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lsp-python-ms" "20200203.946" "The lsp-mode client for Microsoft python-language-server" '((emacs "25.1") (cl-lib "0.6.1") (lsp-mode "6.0")) :stars '(#("40" 0 2 (font-lock-face paradox-star-face))) :commit "396c25c6e0c143c5e5370d9e29b2e65661510e79" :keywords '("languages" "tools") :authors '(("Charl Botha")) :maintainer '("Andrew Christianson, Vincent Zhang") :url "https://github.com/andrew-christianson/lsp-python-ms")
