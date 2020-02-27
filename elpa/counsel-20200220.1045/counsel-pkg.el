115205f5b4de121582818afff094031d58566823
Jonathan Chu <me@jonathanchu.is>
2020-02-26 23:28:44 -0500

    Update 19 packages
    Update cider-20200220.1307 => cider-20200225.1516
    Update counsel-20200220.1045 => counsel-20200224.2036
    Update doom-modeline-20200223.1229 => doom-modeline-20200226.644
    Update doom-themes-20200220.257 => doom-themes-20200225.1641
    Update expand-region-20200106.2058 => expand-region-20200224.1501
    Update flycheck-20191126.2142 => flycheck-20200224.2057
    Update forge-20200217.1909 => forge-20200224.1915
    Update lsp-mode-20200223.935 => lsp-mode-20200226.1707
    Update lsp-python-ms-20200203.946 => lsp-python-ms-20200223.2136
    Update lsp-ui-20200223.1126 => lsp-ui-20200224.743
    Update magit-20200223.1224 => magit-20200226.1310

--- elpa/counsel-20200220.1045/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20200220.1045" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :stars '(#("1168" 0 4 (font-lock-face paradox-star-face))) :commit "db455019dc22a83e9699c087c86eea9248b9bce7" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
---
+++ elpa/org-journal-20200226.1125/org-journal-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-journal" "20200226.1125" "a simple org-mode based journaling mode" '((emacs "25.1")) :stars '(#("443" 0 3 (font-lock-face paradox-star-face))) :commit "d4ff2b012ffe753b7703b9433c1b7bbe7093c721" :authors '(("Bastian Bechtold") ("Christian Schwarzgruber")) :maintainer '("Bastian Bechtold") :url "http://github.com/bastibe/org-journal")
