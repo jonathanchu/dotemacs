4a51c905d971b7b253a8a29d0f5f38a3f60e219c
Jonathan Chu <me@jonathanchu.is>
2025-12-29 16:43:43 -0500

    Migrate web config modes to literate config

--- emacs-init.el
+++ emacs-init.el
@@ -809,6 +809,29 @@ flycheck indicators moved to the right fringe.")
   :config
   (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode)))
 
+(use-package less-css-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.less\\'" . less-css-mode)))
+
+(use-package restclient
+  :ensure t)
+
+(use-package web-mode
+  :ensure t
+  :config
+  (progn
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
+    (add-to-list 'auto-mode-alist '("\\.hb\\.html\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))))
+
 (use-package esup
   :ensure t)
 
