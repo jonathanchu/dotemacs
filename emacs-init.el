3a42036c7397e3282a5a1799e9410c0ec969b0ac
Jonathan Chu <me@jonathanchu.is>
2025-12-29 16:57:13 -0500

    Migrate custom functions to literate config

--- emacs-init.el
+++ emacs-init.el
@@ -409,6 +409,241 @@
   :config
   (add-hook 'prog-mode-hook #'vi-tilde-fringe-mode))
 
+(defun yas/org-very-safe-expand ()
+  (let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
+
+;; make zap-to-char act like zap-up-to-char
+(defadvice zap-to-char (after my-zap-to-char-advice (arg char) activate)
+  "Kill up to the ARG'th occurence of CHAR, and leave CHAR.
+The CHAR is replaced and the point is put before CHAR."
+  (insert char)
+  (forward-char -1))
+
+;; smarter navigation to the beginning of a line
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+     Move point to the first non-whitespace character on this line.
+     If point is already there, move to the beginning of the line.
+     Effectively toggle between the first non-whitespace character and
+     the beginning of the line.
+     If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+     point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;; Write temp files to directory to not clutter the filesystem
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t)
+(setq backup-directory-alist
+      `(("." . ,user-temporary-file-directory)
+        (,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
+
+;; duplicate the current line function
+(defun duplicate-line ()
+  "Duplicate the current line."
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (forward-line 1)
+  (yank))
+
+;; use ido selection for recentf
+(defun ido-choose-from-recentf ()
+  "Use ido to select a recently visited file from the `recentf-list'."
+  (interactive)
+  (find-file (ido-completing-read "Open file: " recentf-list nil t)))
+
+;; swaps windows
+(defun transpose-windows ()
+  "If you have two windows, it swaps them."
+  (interactive)
+  (let ((this-buffer (window-buffer (selected-window)))
+        (other-buffer (prog2
+                          (other-window +1)
+                          (window-buffer (selected-window))
+                        (other-window -1))))
+    (switch-to-buffer other-buffer)
+    (switch-to-buffer-other-window this-buffer)
+    (other-window -1)))
+
+;; Convert word DOuble CApitals to Single Capitals
+(defun dcaps-to-scaps ()
+  "Convert word in DOuble CApitals to Single Capitals."
+  (interactive)
+  (and (= ?w (char-syntax (char-before)))
+       (save-excursion
+         (and (if (called-interactively-p 1)
+                  (skip-syntax-backward "w")
+                (= -3 (skip-syntax-backward "w")))
+              (let (case-fold-search)
+                (looking-at "\\b[[:upper:]]\\{2\\}[[:lower:]]"))
+              (capitalize-word 1)))))
+
+(add-hook 'post-self-insert-hook #'dcaps-to-scaps)
+
+;; Copy the buffer filename to the kill ring
+(defun copy-buffer-file-name-as-kill (choice)
+  "Copy the buffer-file-name to the kill-ring."
+  (interactive "cCopy Buffer Name (f) full, (p) path, (n) name")
+  (let ((new-kill-string)
+        (name (if (eq major-mode 'dired-mode)
+                  (dired-get-filename)
+                (or (buffer-file-name) ""))))
+    (cond ((eq choice ?f)
+           (setq new-kill-string name))
+          ((eq choice ?p)
+           (setq new-kill-string (file-name-directory name)))
+          ((eq choice ?n)
+           (setq new-kill-string (file-name-nondirectory name)))
+          (t (message "Quit")))
+    (when new-kill-string
+      (message "%s copied" new-kill-string)
+      (kill-new new-kill-string))))
+
+;; toggle between most recent buffers
+(defun switch-to-previous-buffer ()
+  "Switch to the most recent buffer.  Toggle back and forth between the two most recent buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+;; toggle window split
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
+;; When popping the mark, continue popping until the cursor actually moves
+;; Also, if the last command was a copy - skip past all the expand-region cruft.
+(defadvice pop-to-mark-command (around ensure-new-position activate)
+  (let ((p (point)))
+    (when (eq last-command #'kill-ring-save)
+      ad-do-it
+      ad-do-it
+      ad-do-it)
+    (dotimes (i 10)
+      (when (= p (point)) ad-do-it))))
+
+(setq set-mark-command-repeat-pop t)
+
+;; Sort directories first in dired-mode
+(defun mydired-sort ()
+  "Sort dired listings with directories first."
+  (save-excursion
+    (let (buffer-read-only)
+      (forward-line 2) ;; beyond dir. header
+      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+    (set-buffer-modified-p nil)))
+
+(defadvice dired-readin
+    (after dired-after-updating-hook first () activate)
+  "Sort dired listings with directories first before adding marks."
+  (mydired-sort))
+
+;; Kill the current buffer
+(defun kill-current-buffer ()
+  "Kills the current buffer"
+  (interactive)
+  (kill-buffer (buffer-name)))
+
+;; transpose the last two words when at end of line
+(defadvice transpose-words
+    (before my/transpose-words)
+  "Transpose the last two words when at the end of line."
+  (if (looking-at "$")
+      (backward-word 1)))
+
+;; Kill the minibuffer when you use the mouse in another window
+;; http://trey-jackson.blogspot.com/2010/04/emacs-tip-36-abort-minibuffer-when.html
+(defun stop-using-minibuffer ()
+  "kill the minibuffer"
+  (when (and (>= (recursion-depth) 1) (active-minibuffer-window))
+    (abort-recursive-edit)))
+
+(add-hook 'mouse-leave-buffer-hook #'stop-using-minibuffer)
+
+(defun my-org-config/after-org-archive ()
+  (org-save-all-org-buffers))
+
+(add-hook 'org-archive-hook 'my-org-config/after-org-archive)
+
+;;----------------------------------------------------------------------------
+;; Key bindings
+;;----------------------------------------------------------------------------
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line] #'smarter-move-beginning-of-line)
+
+;; duplicate the current line
+(global-set-key (kbd "C-c d") #'duplicate-line)
+
+;; recentf with ido selection
+;; bind to infrequently used find-file-read-only.
+;; (global-set-key (kbd "C-x C-r") #'ido-choose-from-recentf)
+
+;; switch to previous buffer
+(global-set-key (kbd "C-`") #'switch-to-previous-buffer)
+
+;; toggle window split
+(global-set-key (kbd "C-x |") #'toggle-window-split)
+
+;; sorting
+(global-set-key (kbd "M-`") #'sort-lines)
+
+;; font-size
+(define-key global-map (kbd "s-=") #'text-scale-increase)
+(define-key global-map (kbd "s--") #'text-scale-decrease)
+
+;; scroll window up/down by one line
+;; (global-set-key (kbd "M-n") (kbd "C-u 1 C-v"))
+;; (global-set-key (kbd "M-p") (kbd "C-u 1 M-v"))
+
+;; fullscreen toggle
+(global-set-key [(s return)] #'toggle-frame-fullscreen)
+
+;; fixup whitespace
+(global-set-key (kbd "C-c w") #'fixup-whitespace)
+
+;; kill the current buffer
+(global-set-key (kbd "C-x C-k") #'kill-current-buffer)
+
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 (add-to-list 'default-frame-alist '(ns-appearance . light))
 
--- init.el
+++ init.el
@@ -38,8 +38,6 @@
 ;; load literate config first
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
 
-(require 'init-funcs)
-
 (require 'elegant)
 
 ;;----------------------------------------------------------------------------
