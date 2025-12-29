b84ef6593fa10a312f9902bfd1478d476381ac64
Jonathan Chu <me@jonathanchu.is>
2025-12-29 16:46:35 -0500

    Migrate paraedit config to literate config

--- emacs-init.el
+++ emacs-init.el
@@ -759,6 +759,21 @@ flycheck indicators moved to the right fringe.")
     (setq sp-autoskip-closing-pair 'always)
     (setq sp-hybrid-kill-entire-symbol nil)))
 
+(use-package paredit
+  :ensure t
+  :config
+  (autoload 'enable-paredit-mode "paredit" t)
+  (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook #'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook #'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook #'enable-paredit-mode)
+  (add-hook 'clojure-mode-hook #'enable-paredit-mode)
+  ;; (add-hook 'org-mode-hook #'enable-paredit-mode)
+  (add-hook 'python-mode-hook
+            (lambda () (local-set-key (kbd "C-k") #'paredit-kill))))
+
 (use-package elpy
   :disabled
   :ensure t
--- init.el
+++ init.el
@@ -38,7 +38,6 @@
 ;; load literate config first
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
 
-(require 'init-paredit)
 (require 'init-highlight)
 (require 'init-smex)
 (require 'init-yasnippet)
