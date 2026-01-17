e493b56e44fa12c85965630a8967b84c0dcc42dc
Jonathan Chu <me@jonathanchu.is>
2026-01-17 15:22:31 -0500

    Remove unnecessary progn wrappers

--- init.el
+++ init.el
@@ -563,11 +563,10 @@
   (flycheck-mode flycheck-list-errors flycheck-buffer)
   :defer 2
   :init
-  (progn
-    (setq flycheck-indication-mode 'right-fringe)
-    (setq flycheck-check-syntax-automatically '(save mode-enabled))
-    (setq flycheck-highlighting-mode 'symbols)
-    (setq flycheck-disabled-checkers '(emacs-lisp emacs-lisp-checkdoc make javascript-jshint)))
+  (setq flycheck-indication-mode 'right-fringe)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (setq flycheck-highlighting-mode 'symbols)
+  (setq flycheck-disabled-checkers '(emacs-lisp emacs-lisp-checkdoc make javascript-jshint))
   :config
   (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
     [0 0 0 0 0 4 12 28 60 124 252 124 60 28 12 4 0 0 0 0])
@@ -701,17 +700,16 @@ flycheck indicators moved to the right fringe.")
 (use-package web-mode
   :ensure t
   :config
-  (progn
-    (setq web-mode-markup-indent-offset 2)
-    (setq web-mode-css-indent-offset 2)
-    (add-to-list 'auto-mode-alist '("\\.hb\\.html\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (add-to-list 'auto-mode-alist '("\\.hb\\.html\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode)))
 
 (use-package markdown-mode)
 
